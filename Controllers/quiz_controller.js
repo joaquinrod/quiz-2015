f8118eaddfc0c59c57d183ccc1167e29c89e30bd
Joaquin Rodriguez <joaquinjrodriguez@hotmail.com>
2015-08-16 19:11:34 -0500

    Autoload

--- Controllers/quiz_controller.js
+++ Controllers/quiz_controller.js
@@ -1,22 +1,15 @@
 var models = require('../models/models.js');
 
-// GET /quizes/:id
-exports.show = function(req, res){
-	models.Quiz.findById(req.params.quizId).then(function(quiz){
-		res.render('quizes/show', {quiz: quiz});
-	})
-};
-
-// GET /quizes/answer
-exports.answer = function(req, res){
-	models.Quiz.findById(req.params.quizId).then(function(quiz){
-		if(req.query.respuesta === quiz.respuesta){
-			res.render('quizes/answer', {quiz: quiz, respuesta: 'Correcto'});
-		}else {
-			res.render('quizes/answer', {quiz: quiz, respuesta: 'Incorrecto'});
-		}
-	})
-};
+// Autoload - factoriza el código si ruta incluye :quizId
+exports.load = function(req, res, next, quizId){
+	models.Quiz.findById(quizId).then(
+		function(quiz){
+			if(quiz){
+				req.quiz = quiz;
+				next();
+			}else {next(new Error('No existe quizId=' + quizId));}
+		}).catch(function(error){ next(error);})
+}
 
 // GET /quizes
 exports.index = function(req, res){
@@ -24,3 +17,22 @@ exports.index = function(req, res){
 		res.render('quizes/index.ejs', {quizes: quizes});
 	})
 };
+
+// GET /quizes/:id
+exports.show = function(req, res){
+	models.Quiz.findById(req.params.quizId).then(function(quiz){
+		res.render('quizes/show', {quiz: req.quiz});
+	})
+};
+
+// GET /quizes/answer
+exports.answer = function(req, res){
+	models.Quiz.findById(req.params.quizId).then(function(quiz){
+		if(req.query.respuesta === quiz.respuesta){
+			res.render('quizes/answer', {quiz: req.quiz, respuesta: 'Correcto'});
+		}else {
+			res.render('quizes/answer', {quiz: req.quiz, respuesta: 'Incorrecto'});
+		}
+	})
+};
+
