b06b80d5ab6836a41be44b9d628daae41cff8a5c
Joaquin Rodriguez <joaquinjrodriguez@hotmail.com>
2015-08-17 13:07:40 -0500

    Crear pregunta

--- Controllers/quiz_controller.js
+++ Controllers/quiz_controller.js
@@ -25,12 +25,8 @@ exports.index = function(req, res){
 	{
 		models.Quiz.findAll().then(function (quizes){
 		res.render('quizes/index.ejs', {quizes: quizes});
-		})
-			
+		})			
 	}
-
-	
-	
 };
 
 // GET /quizes/:id
@@ -51,3 +47,20 @@ exports.answer = function(req, res){
 	})
 };
 
+// GET /quizes/new
+exports.new = function(req, res){
+	var quiz = models.Quiz.build(// crea un objeto tipo quiz
+			{pregunta: "Pregunta", respuesta: "Respuesta"}
+		);
+	res.render('quizes/new', {quiz: quiz});
+}
+
+// POST /quizes/create
+exports.create = function(req, res){
+	var quiz = models.Quiz.build(req.body.quiz);
+
+	// guarda en DB los campos pregunta y respuesta de quiz
+	quiz.save({fields:["pregunta", "respuesta"]}).then(function(){
+		res.redirect('/quizes');
+	}) // Redirección HTTP (URL relativo)
+}
